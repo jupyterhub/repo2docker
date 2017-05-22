2d31b2c289468852fa5e443ccd6d5db34f274c69
yuvipanda <yuvipanda@gmail.com>
2017-05-22 15:15:31 -0700

    Upgrade version of s2i

--- install-s2i.bash
+++ install-s2i.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cd /usr/local/bin/ && curl -L https://github.com/openshift/source-to-image/releases/download/v1.1.5/source-to-image-v1.1.5-4dd7721-linux-amd64.tar.gz | tar xvz
+cd /usr/local/bin/ && curl -L  https://github.com/openshift/source-to-image/releases/download/v1.1.6/source-to-image-v1.1.6-f519129-linux-amd64.tar.gz | tar xvz
 
