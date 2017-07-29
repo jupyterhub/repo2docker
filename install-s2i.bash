a94a857b5f463bd4a6c7987a143fdcc76d1e31e9
yuvipanda <yuvipanda@gmail.com>
2017-07-29 16:41:50 -0700

    Remove s2i script
    s2i is no longer used

--- install-s2i.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cd /usr/local/bin/ && curl -L  https://github.com/openshift/source-to-image/releases/download/v1.1.6/source-to-image-v1.1.6-f519129-linux-amd64.tar.gz | tar xvz
-
