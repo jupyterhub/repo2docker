d5d009ffcab2a87ee51f16c174be716ebd26f373
yuvipanda <yuvipanda@gmail.com>
2017-10-04 17:07:05 -0700

    Build & push docker image from travis

---
+++ travis/travis-script.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu -o pipefail
+
+VERSION=$(git rev-parse --short HEAD)
+
+docker build -t jupyter/repo2docker:${VERSION} .
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    docker login -u ${DOCKER_USERNAME} -p "${DOCKER_PASSWORD}"
+    docker push jupyter/rep2docker:${VERSION}
+fi
