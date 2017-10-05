d962d4091f099b482452ed017e89a5f26a175422
yuvipanda <yuvipanda@gmail.com>
2017-10-04 18:07:09 -0700

    Explicitly echo what image is built

--- travis/travis-script.bash
+++ travis/travis-script.bash
@@ -3,9 +3,11 @@ set -eu -o pipefail
 
 VERSION=$(git rev-parse --short HEAD)
 
-docker build -t jupyter/repo2docker:${VERSION} .
+IMAGE="jupyter/repo2docker:${VERSION}"
+docker build -t ${IMAGE} .
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     docker login -u ${DOCKER_LOGIN} -p "${DOCKER_PASSWORD}"
-    docker push jupyter/rep2docker:${VERSION}
+    docker push ${IMAGE}
+    echo "Pushed new repo2docker image: ${IMAGE}"
 fi
