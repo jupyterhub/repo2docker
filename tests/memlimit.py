43e12631da4a7d5ff13765cfec78acd755e4370a
yuvipanda <yuvipanda@gmail.com>
2017-11-30 20:09:14 -0800

    Make memory limit checking tests more solid
    - Use a more precise way of triggering OOM conditions
    - Make sure that the image is rebuilt each time with a cachebust
    - Don't have two copies of the OOM triggering script

--- tests/memlimit.py
+++ tests/memlimit.py
@@ -1,36 +1,77 @@
+"""
+Test that build time memory limits are actually enforced.
+
+We give the container image at least 128M of RAM (so base things like
+apt and pip can run), and then try to allocate & use 256MB in postBuild.
+This should fail!
+"""
+import os
 import subprocess
+import time
 
-def test_memlimit_nondockerfile():
+def does_build(builddir, mem_limit, mem_allocate_mb):
+    mem_allocate_mb_file = os.path.join(builddir, 'mem_allocate_mb')
+
+    # Cache bust so we actually do a rebuild each time this is run!
+    with open(os.path.join(builddir, 'cachebust'), 'w') as cachebust:
+        cachebust.write(str(time.time()))
+    try:
+        with open(mem_allocate_mb_file, 'w') as f:
+            f.write(str(mem_allocate_mb))
+        try:
+            output = subprocess.check_output(
+                [
+                    'repo2docker',
+                    '--no-run',
+                    '--build-memory-limit', '{}M'.format(mem_limit),
+                    builddir
+                ],
+                stderr=subprocess.STDOUT,
+            ).decode()
+            print(output)
+            return True
+        except subprocess.CalledProcessError as e:
+            output = e.output.decode()
+            print(output)
+            if "The command '/bin/sh -c ./postBuild' returned a non-zero code: 137" in output:
+                return False
+            else:
+                raise
+    finally:
+        os.remove(mem_allocate_mb_file)
+
+
+
+def test_memlimit_nondockerfile_fail():
     """
     Test if memory limited builds are working for non dockerfile builds
     """
-    try:
-        subprocess.check_call([
-            'repo2docker',
-            '--no-run',
-            '--build-memory-limit', '4M',
-            'tests/memlimit/non-dockerfile'
-        ])
-        # If this doesn't throw an exception, then memory limit was
-        # not enforced!
-        assert False
-    except subprocess.CalledProcessError as e:
-        assert True
+    basedir = os.path.dirname(__file__)
+    assert not does_build(
+        os.path.join(basedir, 'memlimit/non-dockerfile'),
+        128,
+        256
+    )
+    assert does_build(
+        os.path.join(basedir, 'memlimit/non-dockerfile'),
+        512,
+        256
+    )
 
 
-def test_memlimit_dockerfile():
+def test_memlimit_dockerfile_fail():
     """
-    Test if memory limited builds are working for non dockerfile builds
+    Test if memory limited builds are working for dockerfile builds
     """
-    try:
-        subprocess.check_call([
-            'repo2docker',
-            '--no-run',
-            '--build-memory-limit', '4M',
-            'tests/memlimit/dockerfile'
-        ])
-        # If this doesn't throw an exception, then memory limit was
-        # not enforced!
-        assert False
-    except subprocess.CalledProcessError as e:
-        assert True
+    basedir = os.path.dirname(__file__)
+    assert not does_build(
+        os.path.join(basedir, 'memlimit/dockerfile'),
+        128,
+        256
+    )
+
+    assert does_build(
+        os.path.join(basedir, 'memlimit/dockerfile'),
+        512,
+        256
+    )
