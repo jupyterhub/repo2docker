e9e6547384a776e3639e08e63f50c77a47918d74
yuvipanda <yuvipanda@gmail.com>
2017-12-19 13:00:55 -0800

    Add tests for user options

---
+++ tests/users.py
@@ -0,0 +1,32 @@
+"""
+Test that User name and ID mapping works
+"""
+import os
+import subprocess
+import tempfile
+import time
+
+def test_user():
+    """
+    Validate user id and name setting
+    """
+    ts = str(time.time())
+    with tempfile.TemporaryDirectory() as tmpdir:
+        username = os.getlogin()
+        subprocess.check_call([
+            'repo2docker',
+            '-v', '{}:/home/{}'.format(tmpdir, username),
+            '--user-id', '1000',
+            '--user-name', 'yuvipanda',
+            tmpdir,
+            '--',
+            '/bin/bash',
+            '-c', 'id -u > id && pwd > pwd && whoami > name'.format(ts)
+        ])
+
+        with open(os.path.join(tmpdir, 'id')) as f:
+            assert f.read().strip() == '1000'
+        with open(os.path.join(tmpdir, 'pwd')) as f:
+            assert f.read().strip() == '/home/yuvipanda'
+        with open(os.path.join(tmpdir, 'name')) as f:
+            assert f.read().strip() == 'yuvipanda'
