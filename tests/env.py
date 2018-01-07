5f95a5ddd3ff07555cf7b5e0c28ed51bcf77c61c
Romain Primet <romain.primet@inria.fr>
2018-01-07 15:43:03 +0000

    Provide a flag to pass environment variables at runtime

---
+++ tests/env.py
@@ -0,0 +1,30 @@
+"""
+Test that volume mounts work when running
+"""
+import os
+import subprocess
+import tempfile
+import time
+
+def test_env():
+    """
+    Validate that you can define environment variables
+    """
+    ts = str(time.time())
+    with tempfile.TemporaryDirectory() as tmpdir:
+        username = os.getlogin()
+        subprocess.check_call([
+            'repo2docker',
+            '-v', '{}:/home/{}'.format(tmpdir, username),
+            '-e', 'FOO={}'.format(ts), 'BAR=baz',
+            '--',
+            tmpdir,
+            '/bin/bash',
+            '-c', 'echo -n $FOO > ts && echo -n $BAR > bar'
+        ])
+
+        with open(os.path.join(tmpdir, 'ts')) as f:
+            assert f.read().strip() == ts
+        with open(os.path.join(tmpdir, 'bar')) as f:
+            assert f.read().strip() == 'baz'
+
