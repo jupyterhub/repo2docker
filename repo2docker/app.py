3ab4503f7c161abc25a809bfe207234aa6471e21
Min RK <benjaminrk@gmail.com>
2018-02-12 13:16:06 +0100

    clone recursively
    ensures submodules, if any, are included

--- repo2docker/app.py
+++ repo2docker/app.py
@@ -151,7 +151,7 @@ class Repo2Docker(Application):
 
     def fetch(self, url, ref, checkout_path):
         try:
-            for line in execute_cmd(['git', 'clone', url, checkout_path],
+            for line in execute_cmd(['git', 'clone', '--recursive', url, checkout_path],
                                     capture=self.json_logs):
                 self.log.info(line, extra=dict(phase='fetching'))
         except subprocess.CalledProcessError:
