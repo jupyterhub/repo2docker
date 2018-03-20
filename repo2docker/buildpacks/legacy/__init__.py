2992409f4a8261ef8ae54c33fe98a6d587c1cc33
Gladys Nalvarte <gladys.programs@gmail.com>
2018-03-20 13:03:51 +0100

    Creates a frozen environment for legacy buildpack.

--- repo2docker/buildpacks/legacy/__init__.py
+++ repo2docker/buildpacks/legacy/__init__.py
@@ -1,8 +1,10 @@
 """
 Generates a variety of Dockerfiles based on an input matrix
 """
+import os
+import shutil
 from textwrap import dedent
-from .docker import DockerBuildPack
+from ..docker import DockerBuildPack
 
 class LegacyBinderDockerBuildPack(DockerBuildPack):
 
@@ -14,13 +16,18 @@ class LegacyBinderDockerBuildPack(DockerBuildPack):
     RUN chown -R main:main /home/main/notebooks
     USER main
     WORKDIR /home/main/notebooks
-    ENV PATH /home/main/anaconda2/envs/python3/bin:$PATH
-    RUN conda install -yq -n python3 notebook==5.0.0 ipykernel==4.6.0 && \
+    # update conda in two steps because the base image
+    # has very old conda that can't upgrade past 4.3
+    RUN conda install -yq conda>=4.3 && \
+        conda install -yq conda==4.4.11 && \
+        conda env update -n python3 -f python3.frozen.yml && \
         conda remove -yq -n python3 nb_conda_kernels && \
-        conda install -yq -n root ipykernel==4.6.0 && \
+        conda env update -n root -f root.frozen.yml && \
         /home/main/anaconda2/envs/python3/bin/ipython kernel install --sys-prefix && \
         /home/main/anaconda2/bin/ipython kernel install --prefix=/home/main/anaconda2/envs/python3 && \
         /home/main/anaconda2/bin/ipython kernel install --sys-prefix
+    RUN rm python3.frozen.yml root.frozen.yml
+    ENV PATH /home/main/anaconda2/envs/python3/bin:$PATH
     ENV JUPYTER_PATH /home/main/anaconda2/share/jupyter:$JUPYTER_PATH
     CMD jupyter notebook --ip 0.0.0.0
     """)
@@ -29,9 +36,22 @@ class LegacyBinderDockerBuildPack(DockerBuildPack):
         with open('Dockerfile') as f:
             return '\n'.join([f.read(), self.legacy_appendix, self.appendix, ''])
 
+    def get_build_script_files(self):
+       return {
+            'legacy/root.frozen.yml': '/tmp/root.frozen.yml',
+            'legacy/python3.frozen.yml': '/tmp/python3.frozen.yml',
+        }
+            
     def build(self, image_spec, memory_limit, build_args):
         with open(self.dockerfile, 'w') as f:
             f.write(self.render())
+        for env in ('root', 'python3'):
+            env_file = env + '.frozen.yml'
+            src_path = os.path.join(
+                os.path.dirname(__file__),
+                env_file,
+            )
+            shutil.copy(src_path, env_file)
         return super().build(image_spec, memory_limit, build_args)
 
     def detect(self):
--- repo2docker/buildpacks/legacy/freeze.py
+++ repo2docker/buildpacks/legacy/freeze.py
@@ -16,25 +16,13 @@ import shutil
 from subprocess import check_call
 import sys
 
-from ruamel.yaml import YAML
-
-
-MINICONDA_VERSION = '4.3.27'
 # need conda ≥ 4.4 to avoid bug adding spurious pip dependencies
-CONDA_VERSION = '4.4.8'
+CONDA_VERSION = '4.4.11'
 
 HERE = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
 
-ENV_FILE = 'environment.yml'
-FROZEN_FILE = os.path.splitext(ENV_FILE)[0] + '.frozen.yml'
 
-ENV_FILE_T = 'environment.py-{py}.yml'
-FROZEN_FILE_T = os.path.splitext(ENV_FILE_T)[0] + '.frozen.yml'
-
-yaml = YAML(typ='rt')
-
-
-def freeze(env_file, frozen_file):
+def freeze(env_name, env_file, frozen_file):
     """Freeze a conda environment.yml
 
     By running in docker:
@@ -56,54 +44,23 @@ def freeze(env_file, frozen_file):
         '--rm',
         '-v' f"{HERE}:/r2d",
         '-it',
-        f"continuumio/miniconda3:{MINICONDA_VERSION}",
+        f"andrewosh/binder-base",
         "sh", "-c",
         '; '.join([
+            "conda update -yq conda",
             f"conda install -yq conda={CONDA_VERSION}",
-            'conda config --add channels conda-forge',
             'conda config --system --set auto_update_conda false',
-            f"conda env create -v -f /r2d/{env_file} -n r2d",
-            # add conda-forge broken channel as lowest priority in case
-            # any of our frozen packages are marked as broken after freezing
-            'conda config --append channels conda-forge/label/broken',
-            f"conda env export -n r2d >> /r2d/{frozen_file}",
+            f"conda env update -f /r2d/{env_file} -n {env_name}",
+            # exclude conda packages because we don't want to pin them
+            f"conda env export -n {env_name} | grep -v conda >> /r2d/{frozen_file}",
         ])
     ])
 
 
-def set_python(py_env_file, py):
-    """Set the Python version in an env file"""
-    if os.path.exists(py_env_file):
-        # only clobber auto-generated files
-        with open(py_env_file) as f:
-            text = f.read()
-            if text and 'GENERATED' not in text:
-                return
-
-    print(f"Regenerating {py_env_file} from {ENV_FILE}")
-    with open(ENV_FILE) as f:
-        env = yaml.load(f)
-    for idx, dep in enumerate(env['dependencies']):
-        if dep.split('=')[0] == 'python':
-            env['dependencies'][idx] = f'python={py}.*'
-            break
-    else:
-        raise ValueError(f"python dependency not found in {env['dependencies']}")
-    # update python dependency
-    with open(py_env_file, 'w') as f:
-        f.write(f"# AUTO GENERATED FROM {ENV_FILE}, DO NOT MANUALLY MODIFY\n")
-        f.write(f"# Generated on {datetime.utcnow():%Y-%m-%d %H:%M:%S UTC}\n")
-        yaml.dump(env, f)
-
-
 if __name__ == '__main__':
     # allow specifying which Pythons to update on argv
-    pys = sys.argv[1:] or ('2.7', '3.5', '3.6')
-    for py in pys:
-        env_file = ENV_FILE_T.format(py=py)
-        set_python(env_file, py)
+    env_names = sys.argv[1:] or ('root', 'python3')
+    for env_name in env_names:
+        env_file = env_name + ".yml"
         frozen_file = os.path.splitext(env_file)[0] + '.frozen.yml'
-        freeze(env_file, frozen_file)
-
-    # use last version as default
-    shutil.copy(frozen_file, FROZEN_FILE)
+        freeze(env_name, env_file, frozen_file)
