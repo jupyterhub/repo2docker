6c19face8e193fee4e6e5484acffe1e5399d4a02
Juan Esteban Arango <juanes.ao@gmail.com>
2018-05-31 12:09:34 -0400

    keep all options except --uid for adduser.

--- repo2docker/buildpacks/base.py
+++ repo2docker/buildpacks/base.py
@@ -36,7 +36,9 @@ ARG NB_UID
 ENV USER ${NB_USER}
 ENV HOME /home/${NB_USER}
 
-RUN adduser ${NB_USER}
+RUN adduser --disabled-password \
+    --gecos "Default user" \
+    ${NB_USER}
 WORKDIR ${HOME}
 
 RUN apt-get update && \
