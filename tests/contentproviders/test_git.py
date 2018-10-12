02b41948ddd6f0169f59b8adbf0bcbba61cd759e
Tim Head <betatim@gmail.com>
2018-10-12 16:28:49 +0200

    Add tests for Git content provider

--- tests/contentproviders/test_git.py
+++ tests/contentproviders/test_git.py
@@ -3,7 +3,7 @@ import shutil
 import os
 import subprocess
 from tempfile import TemporaryDirectory
-from repo2docker.contentproviders.git import GitContentProvider
+from repo2docker.contentproviders.git import Git
 
 
 @contextmanager
@@ -16,23 +16,31 @@ def git_repo():
 
     with TemporaryDirectory() as gitdir:
         subprocess.check_call(['git', 'init'], cwd=gitdir)
-
         yield gitdir
 
+
 def test_clone():
-    """Test simple git clone to a target dir
-    """
+    """Test simple git clone to a target dir"""
     with git_repo() as upstream:
         with open(os.path.join(upstream, 'test'), 'w') as f:
             f.write("Hello")
 
         subprocess.check_call(['git', 'add', 'test'], cwd=upstream)
-        subprocess.check_call(['git', 'commit', '-m', 'Test commit'], cwd=upstream)
+        subprocess.check_call(['git', 'commit', '-m', 'Test commit'],
+                              cwd=upstream)
 
         with TemporaryDirectory() as clone_dir:
-            spec = {
-                'url': upstream
-            }
-            for _ in GitContentProvider().provide(spec, clone_dir, False):
+            spec = {'repo': upstream}
+            for _ in Git().fetch(spec, clone_dir):
                 pass
             assert os.path.exists(os.path.join(clone_dir, 'test'))
+
+
+def test_always_accept():
+    # The git content provider should always accept a spec
+    assert Git().detect('/tmp/doesnt-exist', ref='1234')
+    assert Git().detect('/tmp/doesnt-exist')
+    # a path that exists
+    assert Git().detect('/etc', ref='1234')
+    # a remote URL
+    assert Git().detect('https://example.com/path/here')
