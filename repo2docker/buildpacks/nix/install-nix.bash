046b744785e8d4699c248316b51bf22697a4513e
Tim Head <betatim@gmail.com>
2018-10-19 17:57:40 +0200

    Inherit from BaseImage instead of BuildPack

--- repo2docker/buildpacks/nix/install-nix.bash
+++ repo2docker/buildpacks/nix/install-nix.bash
@@ -5,7 +5,7 @@ set -ex
 NIX_VERSION="2.1.1"
 NIX_SHA256="ad10b4da69035a585fe89d7330037c4a5d867a372bb0e52a1542ab95aec67999"
 
-wget https://nixos.org/releases/nix/nix-$NIX_VERSION/nix-$NIX_VERSION-x86_64-linux.tar.bz2
+wget --quiet https://nixos.org/releases/nix/nix-$NIX_VERSION/nix-$NIX_VERSION-x86_64-linux.tar.bz2
 echo "$NIX_SHA256  nix-2.1.1-x86_64-linux.tar.bz2" | sha256sum -c
 tar xjf nix-*-x86_64-linux.tar.bz2
 sh nix-*-x86_64-linux/install
