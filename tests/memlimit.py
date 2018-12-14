9e5f494c6be37a382e4c11e6128a17703fe4a7d6
yuvipanda <yuvipanda@gmail.com>
2018-12-13 16:55:27 -0800

    Make memory limit tests be unit rather than integration tests
    We don't actually have to validate that docker is doing its
    job. This makes this test much faster

--- tests/memlimit.py
+++ tests/memlimit.py
@@ -1,100 +1,64 @@
 """
-Test that build time memory limits are actually enforced.
+Test that build time memory limits are passed through to docker.
 
-We give the container image at least 128M of RAM (so base things like
-apt and pip can run), and then try to allocate & use 256MB in postBuild.
-This should fail!
+We don't have to actually test if the limit is respected,
+since that is docker's job.
 """
 import os
-import subprocess
-import time
-
-def does_build(builddir, mem_limit, mem_allocate_mb):
-    mem_allocate_mb_file = os.path.join(builddir, 'mem_allocate_mb')
-
-    # Cache bust so we actually do a rebuild each time this is run!
-    with open(os.path.join(builddir, 'cachebust'), 'w') as cachebust:
-        cachebust.write(str(time.time()))
-
-    try:
-        # we don't have an easy way to pass env vars or whatever to
-        # postBuild from here, so we write a file into the repo that is
-        # read by the postBuild script!
-        with open(mem_allocate_mb_file, 'w') as f:
-            f.write(str(mem_allocate_mb))
-        try:
-            output = subprocess.check_output(
-                [
-                    'repo2docker',
-                    '--no-run',
-                    '--build-memory-limit', '{}M'.format(mem_limit),
-                    builddir
-                ],
-                stderr=subprocess.STDOUT,
-            ).decode()
-            print(output)
-            return True
-        except subprocess.CalledProcessError as e:
-            output = e.output.decode()
-            print(output)
-            if "/postBuild' returned a non-zero code: 137" in output:
-                return False
-            else:
-                raise
-    finally:
-        os.remove(mem_allocate_mb_file)
+from unittest.mock import MagicMock
+from tempfile import TemporaryDirectory
+from repo2docker.buildpacks import BaseImage, DockerBuildPack, LegacyBinderDockerBuildPack
+import docker
 
 
+def test_memory_base(monkeypatch):
+    fake_log_value = {'stream': 'fake'}
+    fake_client = MagicMock(spec=docker.APIClient)
+    fake_client.build.return_value = iter([fake_log_value])
 
-def test_memlimit_nondockerfile_fail():
-    """
-    Test if memory limited builds are working for non dockerfile builds
-    """
-    basedir = os.path.dirname(__file__)
-    assert not does_build(
-        os.path.join(basedir, 'memlimit/non-dockerfile'),
-        128,
-        256
-    )
-    assert does_build(
-        os.path.join(basedir, 'memlimit/non-dockerfile'),
-        512,
-        256
-    )
+    with TemporaryDirectory() as d:
+        # Test base image build pack
+        monkeypatch.chdir(d)
+        for line in BaseImage().build(fake_client, 'image-2', '1Gi', {}, []):
+            assert line == fake_log_value
+        _, called_kwargs = fake_client.build.call_args
+        assert 'container_limits' in called_kwargs
+        assert 'memory' in called_kwargs['container_limits']
+        assert called_kwargs['container_limits']['memory'] == '1Gi'
 
 
-def test_memlimit_dockerfile_fail():
-    """
-    Test if memory limited builds are working for dockerfile builds
-    """
-    basedir = os.path.dirname(__file__)
-    assert not does_build(
-        os.path.join(basedir, 'memlimit/dockerfile'),
-        128,
-        256
-    )
+def test_memory_docker(monkeypatch):
+    fake_log_value = {'stream': 'fake'}
+    fake_client = MagicMock(spec=docker.APIClient)
+    fake_client.build.return_value = iter([fake_log_value])
 
-    assert does_build(
-        os.path.join(basedir, 'memlimit/dockerfile'),
-        512,
-        256
-    )
+    with TemporaryDirectory() as d:
+        # Test docker image
+        with open(os.path.join(d, 'Dockerfile'), 'w') as f:
+            f.write('FROM scratch\n')
+        monkeypatch.chdir(d)
+        for line in DockerBuildPack().build(fake_client, 'image-2', '1Gi', {}, []):
+            assert line == fake_log_value
+        _, called_kwargs = fake_client.build.call_args
+        assert 'container_limits' in called_kwargs
+        assert 'memory' in called_kwargs['container_limits']
+        assert called_kwargs['container_limits']['memory'] == '1Gi'
 
+def test_memory_legacy_docker(monkeypatch):
+    fake_log_value = {'stream': 'fake'}
+    fake_client = MagicMock(spec=docker.APIClient)
+    fake_client.build.return_value = iter([fake_log_value])
 
-def test_memlimit_same_postbuild():
-    """
-    Validate that the postBuild files for dockerfile & nondockerfile are same
+    with TemporaryDirectory() as d:
+        monkeypatch.chdir(d)
 
-    Until https://github.com/jupyter/repo2docker/issues/160 gets fixed.
-    """
-    basedir = os.path.dirname(__file__)
-    filepaths = [
-        os.path.join(basedir, 'memlimit', t, "postBuild")
-        for t in ("dockerfile", "non-dockerfile")
-    ]
-    file_contents = []
-    for fp in filepaths:
-        with open(fp) as f:
-            file_contents.append(f.read())
-    # Make sure they're all the same
-    assert len(set(file_contents)) == 1
+        # Test legacy docker image
+        with open(os.path.join(d, 'Dockerfile'), 'w') as f:
+            f.write('FROM andrewosh/binder-base\n')
+
+        for line in LegacyBinderDockerBuildPack().build(fake_client, 'image-2', '1Gi', {}, []):
+            assert line == fake_log_value
+        _, called_kwargs = fake_client.build.call_args
+        assert 'container_limits' in called_kwargs
+        assert 'memory' in called_kwargs['container_limits']
+        assert called_kwargs['container_limits']['memory'] == '1Gi'
