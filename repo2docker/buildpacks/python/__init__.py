38422f1aa793d1f978ff74ac4551ac282a2f31b6
Maksim Rakitin <mrakitin@bnl.gov>
2019-02-07 15:16:30 -0500

    FIX: a temporary workaround for pip-19.0.1 issues

--- repo2docker/buildpacks/python/__init__.py
+++ repo2docker/buildpacks/python/__init__.py
@@ -63,6 +63,7 @@ class PythonBuildPack(CondaBuildPack):
         if os.path.exists(requirements_file):
             assemble_scripts.append((
                 '${NB_USER}',
+                'pip install "pip<19" && ' + \
                 '{} install --no-cache-dir -r "{}"'.format(pip, requirements_file)
             ))
 
