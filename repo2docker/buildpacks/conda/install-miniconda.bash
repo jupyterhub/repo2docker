072fed1dffda61ecc2a4a4ab802a34ccd32d74e0
pughdr <david.pugh@kaust.edu.sa>
2019-06-26 13:57:08 +0300

    Need to add the free channel back into defaults

--- repo2docker/buildpacks/conda/install-miniconda.bash
+++ repo2docker/buildpacks/conda/install-miniconda.bash
@@ -35,6 +35,11 @@ conda config --system --add channels conda-forge
 conda config --system --set auto_update_conda false
 conda config --system --set show_channel_urls true
 
+# conda 4.7 the free channel was removed from defaults; this adds it back while 
+# maintaining correct channel priority (required for the Xeus-Cling test in 
+# test/external/reproductions.repos.yaml to pass) 
+conda config --set restore_free_channel true
+
 # bug in conda 4.3.>15 prevents --set update_dependencies
 echo 'update_dependencies: false' >> ${CONDA_DIR}/.condarc
 
