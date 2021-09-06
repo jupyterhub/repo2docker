yuvipanda/builder

Default to JupyterLab

repo2docker/app.py
@@ -551,6 +551,7 @@ class Repo2Docker(Application):
                 "--port",
                 port,
                 "--NotebookApp.custom_display_url=http://{}:{}".format(host_name, port),
+                "--NotebookApp.default_url=/doc",
             ]
             ports = {"%s/tcp" % port: port}
         else:

jupyterhub/repo2docker

repo2docker/app.py
@@ -582,6 +582,7 @@ class Repo2Docker(Application):
                 "--port",
                 port,
                 "--NotebookApp.custom_display_url=http://{}:{}".format(host_name, port),
+                "--NotebookApp.default_url=/lab",
             ]
             ports = {"%s/tcp" % port: port}
         else: