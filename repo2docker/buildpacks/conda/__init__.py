25bc273a17083e712f7c9944f6aa61c1e886c712
Sylvain Corlay <sylvain.corlay@gmail.com>
2022-01-27 01:02:12 +0100

    Remove -f command-line argument

--- repo2docker/buildpacks/conda/__init__.py
+++ repo2docker/buildpacks/conda/__init__.py
@@ -359,7 +359,7 @@ class CondaBuildPack(BaseImage):
                     "${NB_USER}",
                     r"""
                 ${{MAMBA_EXE}} install -p {0} r-base{1} r-irkernel=1.2 r-devtools -y && \
-                ${{MAMBA_EXE}} clean --all -f -y && \
+                ${{MAMBA_EXE}} clean --all -y && \
                 ${{MAMBA_EXE}} list -p {0}
                 """.format(
                         env_prefix, r_pin
