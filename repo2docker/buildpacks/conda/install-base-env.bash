42f83f0af4e8532f4cf9d719062345a48146d66e
Min RK <benjaminrk@gmail.com>
2022-01-28 15:58:13 +0100

    use mamba for installs once available
    MAMBA_EXE is mamba itself, not micromamba
    use variable everywhere, so switching fully to micromamba can happen in once place,
    assuming they are fully compatible (they _almost_ are, except for `env update` vs `install` in a couple places)

--- repo2docker/buildpacks/conda/install-base-env.bash
+++ repo2docker/buildpacks/conda/install-base-env.bash
@@ -15,14 +15,13 @@ URL="https://anaconda.org/conda-forge/micromamba/${MAMBA_VERSION}/download/linux
 unset HOME
 mkdir -p ${CONDA_DIR}
 
-export MAMBA_ROOT_PREFIX=${CONDA_DIR}
-export MAMBA_EXE="/usr/local/bin/micromamba"
+export MICROMAMBA_EXE="/usr/local/bin/micromamba"
 
 time wget -qO- ${URL} | tar -xvj bin/micromamba
-mv bin/micromamba "$MAMBA_EXE"
-chmod 0755 "$MAMBA_EXE"
+mv bin/micromamba "$MICROMAMBA_EXE"
+chmod 0755 "$MICROMAMBA_EXE"
 
-eval "$(${MAMBA_EXE} shell hook -p ${CONDA_DIR} -s posix)"
+eval "$(${MICROMAMBA_EXE} shell hook -p ${CONDA_DIR} -s posix)"
 
 micromamba activate
 
@@ -44,7 +43,7 @@ echo "installing notebook env:"
 cat "${NB_ENVIRONMENT_FILE}"
 
 
-time micromamba create -p ${NB_PYTHON_PREFIX} -f "${NB_ENVIRONMENT_FILE}"
+time ${MAMBA_EXE} create -p ${NB_PYTHON_PREFIX} --file "${NB_ENVIRONMENT_FILE}"
 
 if [[ ! -z "${NB_REQUIREMENTS_FILE:-}" ]]; then
     echo "installing pip requirements"
@@ -61,7 +60,7 @@ if [[ ! -z "${KERNEL_ENVIRONMENT_FILE:-}" ]]; then
     # install kernel env and register kernelspec
     echo "installing kernel env:"
     cat "${KERNEL_ENVIRONMENT_FILE}"
-    time micromamba create -p ${KERNEL_PYTHON_PREFIX} -f "${KERNEL_ENVIRONMENT_FILE}"
+    time ${MAMBA_EXE} create -p ${KERNEL_PYTHON_PREFIX} --file "${KERNEL_ENVIRONMENT_FILE}"
 
     if [[ ! -z "${KERNEL_REQUIREMENTS_FILE:-}" ]]; then
         echo "installing pip requirements for kernel"
@@ -71,18 +70,18 @@ if [[ ! -z "${KERNEL_ENVIRONMENT_FILE:-}" ]]; then
 
     ${KERNEL_PYTHON_PREFIX}/bin/ipython kernel install --prefix "${NB_PYTHON_PREFIX}"
     echo '' > ${KERNEL_PYTHON_PREFIX}/conda-meta/history
-    micromamba list -p ${KERNEL_PYTHON_PREFIX}
+    ${MAMBA_EXE} list -p ${KERNEL_PYTHON_PREFIX}
 fi
 
 # Clean things out!
-time micromamba clean --all -y
+time ${MAMBA_EXE} clean --all -f -y
 
 # Remove the pip cache created as part of installing micromamba
 rm -rf /root/.cache
 
 chown -R $NB_USER:$NB_USER ${CONDA_DIR}
 
-micromamba list -p ${NB_PYTHON_PREFIX}
+${MAMBA_EXE} list -p ${NB_PYTHON_PREFIX}
 
 # Set NPM config
 ${NB_PYTHON_PREFIX}/bin/npm config --global set prefix ${NPM_DIR}
