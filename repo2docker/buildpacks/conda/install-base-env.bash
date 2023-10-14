4b0c7d9bfda9650185aa8c9c0eb25eef136831d9
Simon Li <orpheus+devel@gmail.com>
2023-10-14 23:12:58 +0100

    conda 23.7.4
    error    libmamba Could not solve for environment specs
        The following packages are incompatible
        ├─ conda 23.9.0**  is requested and can be installed;
        └─ mamba 1.5.1**  is not installable because it requires
           └─ conda >=4.14,<23.9.0a0 , which conflicts with any installable versions previously reported.

--- repo2docker/buildpacks/conda/install-base-env.bash
+++ repo2docker/buildpacks/conda/install-base-env.bash
@@ -6,7 +6,7 @@ set -ex
 cd $(dirname $0)
 
 export MAMBA_VERSION=1.5.1
-export CONDA_VERSION=23.9.0
+export CONDA_VERSION=23.7.4
 
 URL="https://anaconda.org/conda-forge/micromamba/${MAMBA_VERSION}/download/${CONDA_PLATFORM}/micromamba-${MAMBA_VERSION}-0.tar.bz2"
 
