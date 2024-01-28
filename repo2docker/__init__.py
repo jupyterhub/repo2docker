fa8394b09bfc70e2fca3e8b4d8af1e823761400e
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2024-01-28 15:02:03 +0100

    Run pre-commit

--- repo2docker/__init__.py
+++ repo2docker/__init__.py
@@ -3,7 +3,7 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
+from . import _version
 from .app import Repo2Docker
 
-from . import _version
-__version__ = _version.get_versions()['version']
+__version__ = _version.get_versions()["version"]
