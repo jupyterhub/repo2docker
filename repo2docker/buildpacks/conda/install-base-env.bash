920ddd86ac55add4e8db22a27086e248f223cd7e
Erik Sundell <erik.i.sundell@gmail.com>
2024-03-24 22:16:05 +0100

    Update conda from 23.7.4 to 24.3.0

--- repo2docker/buildpacks/conda/install-base-env.bash
+++ repo2docker/buildpacks/conda/install-base-env.bash
@@ -6,7 +6,7 @@ set -ex
 cd $(dirname $0)
 
 export MAMBA_VERSION=1.5.7
-export CONDA_VERSION=23.7.4
+export CONDA_VERSION=24.3.0
 
 URL="https://anaconda.org/conda-forge/micromamba/${MAMBA_VERSION}/download/${CONDA_PLATFORM}/micromamba-${MAMBA_VERSION}-0.tar.bz2"
 
