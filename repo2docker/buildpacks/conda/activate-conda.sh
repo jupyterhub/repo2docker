0a14433ea8595670f7f288d07898344c656218b6
Julien Jerphanion <git@jjerphan.xyz>
2024-08-22 17:31:03 +0200

    Use `micromamba activate` instead of `mamba activate`
    The shell macros are only defined for micromamba
    but not for mamba.
    
    This brings consistency.
    
    But then, what was making it working before?
    
    Signed-off-by: Julien Jerphanion <git@jjerphan.xyz>
    
    Co-authored-by: Sylvain Corlay <sylvain.corlay@gmail.com>
    Co-authored-by: Hind Montassif <hind.montassif@gmail.com>

--- repo2docker/buildpacks/conda/activate-conda.sh
+++ repo2docker/buildpacks/conda/activate-conda.sh
@@ -7,8 +7,8 @@ done
 if [[ "${KERNEL_PYTHON_PREFIX}" != "${NB_PYTHON_PREFIX}" ]]; then
     # if the kernel is a separate env, stack them
     # so both are on PATH, notebook first
-    mamba activate ${KERNEL_PYTHON_PREFIX}
-    mamba activate --stack ${NB_PYTHON_PREFIX}
+    micromamba activate ${KERNEL_PYTHON_PREFIX}
+    micromamba activate --stack ${NB_PYTHON_PREFIX}
 
     # even though it's second on $PATH
     # make sure CONDA_DEFAULT_ENV is the *kernel* env
@@ -17,5 +17,5 @@ if [[ "${KERNEL_PYTHON_PREFIX}" != "${NB_PYTHON_PREFIX}" ]]; then
     # which only contains UI when the two are different
     export CONDA_DEFAULT_ENV="${KERNEL_PYTHON_PREFIX}"
 else
-    mamba activate ${NB_PYTHON_PREFIX}
+    micromamba activate ${NB_PYTHON_PREFIX}
 fi
