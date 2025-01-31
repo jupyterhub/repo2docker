e1444c26210585079bfc2064d11e418f0e336c92
Julien Jerphanion <git@jjerphan.xyz>
2025-01-31 13:53:35 +0100

    Use 2.0.6.rc2

--- repo2docker/buildpacks/conda/install-base-env.bash
+++ repo2docker/buildpacks/conda/install-base-env.bash
@@ -5,7 +5,7 @@ set -ex
 
 cd $(dirname $0)
 
-export MAMBA_VERSION="2.0.6.rc0"
+export MAMBA_VERSION="2.0.6.rc2"
 export CONDA_VERSION=24.11.0
 
 URL="https://anaconda.org/conda-forge/micromamba/${MAMBA_VERSION}/download/${CONDA_PLATFORM}/micromamba-${MAMBA_VERSION}-0.tar.bz2"
