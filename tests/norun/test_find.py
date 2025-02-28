56cf063636c589475f3f19e39df5a6b9619f3c0f
YuviPanda <yuvipanda@gmail.com>
2025-02-28 14:28:21 -0800

    Don't pass --no-run to find tests

--- tests/norun/test_find.py
+++ tests/norun/test_find.py
@@ -8,16 +8,16 @@ HERE = Path(__file__).parent
 
 def test_find_image():
     image_name = f"{secrets.token_hex(8)}:latest"
-    r2d = make_r2d(["--image", image_name, "--no-run", str(HERE)])
+    r2d = make_r2d(["--image", image_name, str(HERE)])
 
-    r2d.start()
+    r2d.build()
 
     assert r2d.find_image()
 
 
 def test_dont_find_image():
     image_name = f"{secrets.token_hex(8)}:latest"
-    r2d = make_r2d(["--image", image_name, "--no-run", str(HERE)])
+    r2d = make_r2d(["--image", image_name, str(HERE)])
 
     # Just don't actually start the build, so image won't be found
     assert not r2d.find_image()
