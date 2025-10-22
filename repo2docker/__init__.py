a6e02db5472c360381b43d7a8d6a0ed4bc2ccafd
Min RK <benjaminrk@gmail.com>
2025-10-21 21:55:05 -0700

    finish updating to pyproject.toml
    removes:
    
    - MANIFEST.in (handled by setuptools-scm)
    - setup.py (declarative in pyproject.toml)
    - versioneer (replaced with setuptools-scm)
    - setup.cfg (was for versioneer)

--- repo2docker/__init__.py
+++ repo2docker/__init__.py
@@ -1,4 +1,8 @@
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
 from .app import Repo2Docker
 
 # You can add this if you want an __all__ variable to control imports
